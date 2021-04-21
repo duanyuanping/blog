25d2fe1d72d1000744e2b929f826a0f16cd0caaf
walterduan <walterduan@tencent.com>
2021-04-21 10:05:50 +0800

    update: 部分内容更新

--- js/index.js
+++ js/index.js
@@ -1,8 +1,56 @@
-new Promise((res) => {
-  console.log('1')
-  res(2)
-}).then(data => console.log(data))
+class MyEventEmitter {
+  constructor() {
+    this._events = Object.create(null);
 
-process.nextTick(() => {
-  console.log(3)
-})
+    this.on = this.addListener;
+  }
+
+  addListener(type, callback, flag) {
+    if (!this._events) this._events = Object.create(null);
+
+    if (this._events[type]) {
+      if (flag) {
+        this._events[type].unshift(callback);
+      } else {
+        this._events[type].push(callback);
+      }
+    } else {
+      this._events[type] = [callback];
+    }
+  }
+
+  once(type, callback, flag) {
+    function newCallback(...args) {
+      callback.apply(this, args);
+      this.removeListen(type, newCallback);
+    }
+
+    this.addListener(type, newCallback, flag);
+  }
+
+  removeListen(type, callback) {
+    if (this._events[type]) {
+      this._events[type] = this._events[type].filter(fn => fn !== callback);
+    }
+  }
+
+  emit(type, ...value) {
+    if (!this._events[type]) return;
+
+    this._events[type].forEach(fn => fn.apply(this, value));
+  }
+}
+
+const events = new MyEventEmitter();
+const onFn = () => {
+  console.log('on1');
+};
+const onceFn = () => {
+  console.log('once2');
+};
+events.on('test', onFn);
+events.once('test', onceFn);
+events.emit('test');
+console.log('+++++++++')
+// events.removeListen('test', onFn)
+events.emit('test');
